8427035fcc1a803e4a8ee3171a8d00d3d638443a
Phil Lee <phil@8power.com>
2020-11-13 09:11:08 +0000

    Issue-147: Remove redundant settings

--- ublox-constants.go
+++ ublox-constants.go
@@ -75,10 +75,6 @@ const setOobTemporaryKey = "+UBTOTK"
 const oobTemporaryKey = "00112233445566778899AABBCCDDEEFF"
 const setOobSecurityMode = "+UBTSM"
 const securityDisabled uint = 1
-const securityEnabled uint = 2
-const securityEnabledDisplayOnly uint = 3
-const securityEnabledDisplayYesNo uint = 4
-const securityEnabledDisplayKeyboardOnly uint = 5
 const securityEnabledOutOfBand uint = 6
 
 const readEscapeCharacter = "S2"
